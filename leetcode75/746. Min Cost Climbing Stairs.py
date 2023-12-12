35cc3fd829462cba40cac4709c054ec639ebbc2d
Chao (Glen) Xu <59375616+ChaoGlenXu@users.noreply.github.com>
2023-12-11 23:56:07 -0500

    Create 746. Min Cost Climbing Stairs.py

---
+++ leetcode75/746. Min Cost Climbing Stairs.py
@@ -0,0 +1,11 @@
+#746. Min Cost Climbing Stairs
+#Easy
+#problem statement:   https://leetcode.com/problems/min-cost-climbing-stairs/
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        for i in range(len(cost)-3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+
+        return min(cost[0], cost[1])
