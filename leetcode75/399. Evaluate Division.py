99f46e3c0a30afe1775374545737f287e7438286
Chao (Glen) Xu <59375616+ChaoGlenXu@users.noreply.github.com>
2025-01-17 20:06:09 -0800

    Update 399. Evaluate Division.py

--- leetcode75/399. Evaluate Division.py
+++ leetcode75/399. Evaluate Division.py
@@ -5,24 +5,24 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         adj = defaultdict(dict)
-        #self.visited = set() this gobal way can cause infinite loop in this case, so this should be a variable passed into dfs 
+        #self.visited = set() this gobal way can cause infinite loop in this case, so this should be a variable pass in dfs 
         for idx, eq in enumerate(equations):
             a, b = eq 
             adj[a][b] = values[idx]
             adj[b][a] = 1 / values[idx]
-        #print(adj)
+        #print(adj) #for debuging
         def dfs(src, target, visited): 
+            #print(src + " " +  target + " " + str(visited)) #for debuging
             if (src not in adj) or (target not in adj): return -1
             if src == target: return 1
             visited.add(src)
             for each in adj[src]:
                 if each not in visited:
                     result = dfs( each, target, visited)
-                    if result != -1:
-                        return result * adj[src][each] 
-            return -1
+                    if result != -1:  #-------------------
+                        return result * adj[src][each]  #|
+            return -1 #<---------------------------------- preventing this case
         return [dfs(q[0], q[1], set())for q in queries]
-
 
         '''
         if each == target:   
