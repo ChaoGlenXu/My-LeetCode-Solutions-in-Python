0349e13b38c7a643194d89f3881b4195d2d1f2f9
Chao (Glen) Xu <59375616+ChaoGlenXu@users.noreply.github.com>
2025-02-23 08:00:20 -0800

    Create 72. Edit Distance.py

---
+++ leetcode75/72. Edit Distance.py
@@ -0,0 +1,20 @@
+#72. Edit Distance
+#Medium
+#problem statement:   https://leetcode.com/problems/edit-distance/description/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        res, c2, len1, len2= 0, 0, len(word1), len(word2)
+        if len2 == 0: return len1
+        elif len1 == 0: return len2
+        dp = [[0 for i in range(len1+1)] for j in range(len2+1)]
+        
+        for y in range(len2, -1, -1): dp[y][len1] = len2-y 
+        for x in range(len1, -1, -1): dp[len2][x] = len1-x
+        #print(dp)
+
+        for y in range(len2-1, -1, -1):
+            for x in range(len1-1, -1, -1):
+                if word1[x] == word2[y]:dp[y][x] = dp[y+1][x+1]
+                else: dp[y][x] = 1 + min(dp[y][x+1], dp[y+1][x], dp[y+1][x+1])
+        return dp[0][0]
