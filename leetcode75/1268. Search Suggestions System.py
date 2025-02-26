b14c1a87470a8d20881dcd9d88fd0a545978443e
Chao (Glen) Xu <59375616+ChaoGlenXu@users.noreply.github.com>
2025-02-26 11:42:01 -0800

    Update 1268. Search Suggestions System.py

--- leetcode75/1268. Search Suggestions System.py
+++ leetcode75/1268. Search Suggestions System.py
@@ -1,1 +1,48 @@
 #1268. Search Suggestions System
+#Medium
+#problem statement:   https://leetcode.com/problems/search-suggestions-system/description/?envType=study-plan-v2&envId=leetcode-75
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+        self.suggestions = []
+
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def build(self, products: List[str]):
+        products.sort()
+        
+        for pro in products:
+            cur = self.root
+            for c in pro:
+                if c not in cur.children: cur.children[c] = TrieNode()
+                cur = cur.children[c]
+                if len(cur.suggestions) < 3 : cur.suggestions.append(pro)
+            cur.word = True
+
+    def search (self, searchWord: str) -> List[List[str]]:
+        res , len_sea = [], len(searchWord)
+        cur = self.root
+
+        for c in searchWord:
+            if c not in cur.children: break
+            cur = cur.children[c]
+            res.append(cur.suggestions)
+
+        while len(res) < len_sea: res.append([])
+
+        return res     
+
+
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+
+        #trie approach
+        trie = Trie()
+        trie.build(products)
+        return trie.search(searchWord)
+
