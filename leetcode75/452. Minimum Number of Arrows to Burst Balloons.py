c904b87cf4cb1a06920b9dd45f481a5385b4855c
Chao (Glen) Xu <59375616+ChaoGlenXu@users.noreply.github.com>
2025-02-27 13:00:47 -0800

    Update 452. Minimum Number of Arrows to Burst Balloons.py

--- leetcode75/452. Minimum Number of Arrows to Burst Balloons.py
+++ leetcode75/452. Minimum Number of Arrows to Burst Balloons.py
@@ -4,13 +4,27 @@
 
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
+        #concise code without comment
+        points.sort()
+        res, pre = len(points), points[0]
+
+        for start, end in points[1:]:
+            if start > pre[1]: pre = [start, end]
+            else:
+                res -= 1
+                pre = [start, min(pre[1], end)]
+        
+        return res
+
+        '''
         points.sort()
         res, pre = len(points), points[0]
 
         for start, end in points[1:]:
             if start > pre[1]: 
-                pre[0] = start
-                pre[1] = end
+                pre = [start, end] #6 % faster than the following two line
+                #pre[0] = start
+                #pre[1] = end
             else:
                 res -= 1
                 pre = [start, min(pre[1], end)] #Runtime 165ms Beats 26.95% Memory 51.50MB Beats 34.77%
@@ -19,11 +33,9 @@ class Solution:
                 #pre[1] = min(pre[1], end)
         
         return res
+        '''
 
-
-        #all the following the commented out code are wrong and just keep track of the mistake that i made, 
-        #the mistate i made the following is that i only kept track of pre_end, but should also keep track 
-        #the current overlapping interval, the solution i did above was done after checking neetcode video explanation
+        #all the following the commented out code are wrong and just keep track of the mistake that i made, the mistate i made the following is that i only kept track of pre_end, but should also keep track the current overlapping interval, the solution i did above was done after checking neetcode video explanation
         '''
         res, pre_end, low_inter_end = len(points), points[0][1], points[0][1]
         print(points)
