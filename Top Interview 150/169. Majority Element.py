a898060dd07723a4798b58eb18b52e18c1fb371d
Chao (Glen) Xu <59375616+ChaoGlenXu@users.noreply.github.com>
2025-03-05 15:32:05 -0800

    Update 169. Majority Element.py
    #solve it with the most optimal-Boyer-Moore Voting Algorithm approach

--- Top Interview 150/169. Majority Element.py
+++ Top Interview 150/169. Majority Element.py
@@ -4,6 +4,25 @@
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+
+        #most optimal-Boyer-Moore Voting Algorithm
+        res, c = 0, 0
+        for i in nums:
+            if c == 0: res = i
+            c += (1 if res == i else -1)
+        return res
+
+        '''
+        #impoved version:
+        d , maj , half = {}, [nums[0], 1], len(nums)/2
+        for i in nums:
+            d[i] = 1 + d.get(i, 0)
+            if d[i] > maj[1]: maj[0], maj[1] = i, d[i]
+            if maj[1] > half: break #improve time complexity a little bit
+        return maj[0] 
+        '''
+
+        '''
         d , maj , half = {}, [nums[0], 1], len(nums)/2
         for i in nums:
             if i not in d: d[i] = 1
@@ -12,4 +31,5 @@ class Solution:
                 if d[i] > maj[1]: maj[0], maj[1] = i, d[i]
                 if maj[1] > half: break #improve time complexity a little bit
         return maj[0] 
+        '''
         
