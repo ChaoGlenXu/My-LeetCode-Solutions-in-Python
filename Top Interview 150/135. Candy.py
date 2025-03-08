f74666da0934ea7ea9a29db31ade7d85e2cbfa57
Chao (Glen) Xu <59375616+ChaoGlenXu@users.noreply.github.com>
2025-03-08 00:50:53 -0800

    Update 135. Candy.py
    #more concise version after removing the variable - more

--- Top Interview 150/135. Candy.py
+++ Top Interview 150/135. Candy.py
@@ -4,6 +4,20 @@
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        #more concise version after removing the variable - more
+        candies = [1] * len(ratings)
+
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                candies[i] = candies[i-1] + 1
+ 
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candies[i] = max(candies[i], candies[i+1] + 1)
+
+        return sum(candies)
+
+        '''        
         #I solved this by myself for around 58 minutes (Top Interview 150)
         candies = [1] * len(ratings)
 
@@ -24,3 +38,4 @@ class Solution:
             else: more = 0
         #print(candies)
         return sum(candies)
+        '''
